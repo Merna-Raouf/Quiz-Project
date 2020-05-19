e6d5f4a2da1c226c9a4a4a469d792197e4bbba14
Merna-Raouf <36385493+Merna-Raouf@users.noreply.github.com>
2020-05-20 01:11:29 +0200

    updating main

--- getQuiz.py
+++ getQuiz.py
@@ -1,62 +1,61 @@
 from model import *
-
+import time
+import signal
 def getQuiz():
     """
     it allows the student to take the quiz and gets his mark
     :return:
     """
+    start = time.time()
     Total=0
     Quizes = session.query(Quiz).all()
     for quiz in Quizes:
-        print(quiz.name)
-    name=input("Enter the name of the quiz: ")
+        print("{} . {} ".format(quiz.id,quiz.name))
+    ID= input("Enter the id of the quiz: ")
 
-    for quiz in Quizes:
-        if (quiz.name==name):
-            print(f"The Duration of the quiz is : {quiz.Duration} minutes")
-            print("Let's start the quiz!")
+    quiz=session.query(Quiz).filter(Quiz.id == int(ID))
+    for row in quiz:
+        print(f"The Duration of the quiz is : {row.Duration} minutes")
+        print("Let's start the quiz!")
+        Mcq=session.query(MCQ).filter(MCQ.Quiz_id == int(ID))
+        for q in Mcq :
+            print(q.Question)
+            print(q.first_Choice)
+            print(q.second_Choice)
+            print(q.third_Choice)
+            print(q.fourth_Choice)
+            answer = input("Answer: ")
+            if(answer==q.answer):
+                Total+=q.mark
 
-            Mcq=session.query(MCQ).all()
-            for q in Mcq:
-                if(q.Quiz_id==quiz.id):
-                    print(q.Question)
-                    print(q.first_Choice)
-                    print(q.second_Choice)
-                    print(q.third_Choice)
-                    print(q.fourth_Choice)
-                    answer=input("Answer: ")
-                    if(answer==q.answer):
-                        Total+=q.mark
+        Trueorfalse = session.query(TrueOrFalse).filter(TrueOrFalse.Quiz_id == int(ID))
+        for q in Trueorfalse:
+            print(q.Question)
+            answer = input("Answer: ")
+            if (bool(answer) == q.answer):
+                Total += q.mark
 
-            Trueorfalse=session.query(TrueOrFalse).all()
-            for q in Trueorfalse:
-                if (q.Quiz_id == quiz.id):
-                    print(q.Question)
-                    answer = input("Answer: ")
-                    if (bool(answer) == q.answer):
-                        Total += q.mark
 
-            Essay = session.query(Essay_Question).all()
-            for q in Essay:
-                if (q.Quiz_id == quiz.id):
-                    print(q.Question)
-                    answer = input("Answer: ")
-                    if (answer == q.answer):
-                        Total += q.mark
+        Essay = session.query(Essay_Question).filter(Essay_Question.Quiz_id == int(ID))
+        for q in Essay:
+            print(q.Question)
+            answer = input("Answer: ")
+            if (answer == q.answer):
+                Total += q.mark
 
-            integer_qus = session.query(Integer_Question).all()
-            for q in integer_qus:
-                if (q.Quiz_id == quiz.id):
-                    print(q.Question)
-                    answer = input("Answer: ")
-                    if (int(answer) == q.answer):
-                        Total += q.mark
+        integer_qus = session.query(Integer_Question).filter(Integer_Question.Quiz_id == int(ID))
+        for q in integer_qus:
+            print(q.Question)
+            answer = input("Answer: ")
+            if (int(answer) == q.answer):
+                Total += q.mark
 
-            float_qus = session.query(Float_Question).all()
-            for q in float_qus:
-                if (q.Quiz_id == quiz.id):
-                    print(q.Question)
-                    answer = input("Answer: ")
-                    if (float(answer) == q.answer):
-                        Total += q.mark
-            print(f"Mark: {Total}")
+        float_qus = session.query(Float_Question).filter(Float_Question.Quiz_id == int(ID))
+        for q in float_qus:
+            print(q.Question)
+            answer = input("Answer: ")
+            if (float(answer) == q.answer):
+                Total += q.mark
+
+        print(f"Mark: {Total}")
+
--- main.py
+++ main.py
@@ -1,17 +1,44 @@
+import sys
 from addQuiz import *
 from getQuiz import *
+from time import time
+import multiprocessing
 
-def main():
-    mode=input("""Select your mode:
-    Enter 1 for Teacher
-    Enter 2 for Student
-    """)
 
-    if (mode=='1'):
+def program():
+    mode = input("""Select your mode:
+        Enter 1 for Teacher
+        Enter 2 for Student
+        """)
+
+    if (mode == '1'):
         Add_Quiz()
 
-    elif(mode=='2') :
+    elif (mode == '2'):
         getQuiz()
 
+
+def main():
+    # Start foo as a process
+    p = multiprocessing.Process(target=program,name="program",args=())
+    p.start()
+
+    # Wait 10 seconds for foo
+    time.sleep(50)
+
+    # Terminate foo
+    p.terminate()
+
+    # Cleanup
+    p.join()
+
+
+def timer():
+    duration=program()
+    start = time.time()
+    if (time.time() - start <duration * 60):
+        sys.exit()
+
+
 if __name__ == '__main__':
-    main()
+  main()
